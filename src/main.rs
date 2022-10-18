0e730805dd91a9c4ec3506e0ca5efb0cd31273c3
Aaron Vandeweghe <aaron.vandeweghe@student.hogent.be>
2022-10-18 14:58:55 +0200

    added image POST functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use std::{fmt::format, io::stdout};
-
+use rocket::fs::{TempFile, FileServer, relative};
+use rocket::form::Form;
 #[macro_use] extern crate rocket;
 // #[macro_use] extern crate serde_json;
 // static mut name: &str = "World";
+
+#[derive(FromForm)]
+struct Upload<'r> {
+    save : bool,
+    file: TempFile<'r>,
+}
+//  persist to needs a file not directory
+//  write function to determine name
+#[post("/upload",format = "multipart/form-data", data="<upload>")]
+async fn upload_form(mut upload: Form<Upload<'_>>) -> std::io::Result<()> {
+    upload.file.persist_to("C:\\Users\\aaron\\Desktop\\temp\\file.png").await?;
+    Ok(())
+}
 
 #[get("/")]
 fn index() -> String {
@@ -19,5 +32,7 @@ fn create_index(name : String) -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/" , routes![index, create_index])
+    rocket::build()
+    .mount("/" , routes![index, create_index, upload_form])
+    .mount("/public", FileServer::from(relative!("/static/images")))
 }
